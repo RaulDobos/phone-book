2a80a88d848803d41c21f94ce6dd977c5e9ad836
RaulDobos <raulpriam@yahoo.com>
2020-06-22 12:25:18 +0300

    implemented CRUD methods in contact repository

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,10 +1,14 @@
 package org.fasttrackit;
 
+import org.fasttrackit.domain.Contact;
 import org.fasttrackit.persistance.ContactRepository;
 import org.fasttrackit.transfer.CreateContactRequest;
+import org.fasttrackit.transfer.UpdateContactRequest;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Hello world!
@@ -13,13 +17,24 @@ import java.sql.SQLException;
 public class App 
 {
     public static void main( String[] args ) throws IOException, SQLException, ClassNotFoundException {
-        CreateContactRequest request = new CreateContactRequest();
-        request.setFirstName("Raul");
-        request.setLastName("Dobos");
-        request.setPhoneNumber("0722201278");
+//        CreateContactRequest request = new CreateContactRequest();
+//        request.setFirstName("Flaviu");
+//        request.setLastName("Ratiu");
+//        request.setPhoneNumber("0723913123");
 
         ContactRepository contactRepository = new ContactRepository();
-        contactRepository.createContact(request);
+//        contactRepository.createContact(request);
 
+//        UpdateContactRequest updateRequest = new UpdateContactRequest();
+//        updateRequest.setFirstName("Flaviu");
+//        updateRequest.setLastName("Ratiu");
+//        updateRequest.setPhoneNumber("2342342322");
+
+//        contactRepository.updateContact(1, updateRequest);
+
+//        contactRepository.deleteTask(1);
+
+        List<Contact> contacts = contactRepository.getContacts();
+        System.out.println(contacts);
     }
 }
--- src/main/java/org/fasttrackit/persistance/ContactRepository.java
+++ src/main/java/org/fasttrackit/persistance/ContactRepository.java
@@ -1,11 +1,14 @@
 package org.fasttrackit.persistance;
 
+import javafx.concurrent.Task;
+import org.fasttrackit.domain.Contact;
 import org.fasttrackit.transfer.CreateContactRequest;
+import org.fasttrackit.transfer.UpdateContactRequest;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactRepository {
 
@@ -23,4 +26,61 @@ public class ContactRepository {
             preparedStatement.executeUpdate();
         }
     }
+
+    public void updateContact(long id, UpdateContactRequest request) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "UPDATE contact SET first_name=?, last_name=?, phone_number=? WHERE id=?";
+
+        try(Connection connection = DatabaseConfiguration.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+            preparedStatement.setString(1, request.getFirstName());
+            preparedStatement.setString(2, request.getLastName());
+            preparedStatement.setString(3, request.getPhoneNumber());
+            preparedStatement.setLong(4, id);
+
+            preparedStatement.executeUpdate();
+        }
+
+    }
+
+    public void deleteTask(long id) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "DELETE FROM contact WHERE id=?;";
+
+        try(Connection connection = DatabaseConfiguration.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+            preparedStatement.setLong(1, id);
+
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public List<Contact> getContacts() throws SQLException, IOException, ClassNotFoundException {
+        String sql = "SELECT id, first_name, last_name, phone_number FROM contact";
+
+        List<Contact> contacts = new ArrayList<>();
+
+        try(Connection connection = DatabaseConfiguration.getConnection();
+            Statement statement = connection.createStatement()){
+
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while(resultSet.next()){
+                Contact contact = new Contact();
+
+                contact.setId(resultSet.getLong("id"));
+                contact.setFirstName(resultSet.getString("first_name"));
+                contact.setLastName(resultSet.getString("last_name"));
+                contact.setPhoneNumber(resultSet.getString("phone_number"));
+
+                contacts.add(contact);
+            }
+        }
+
+        return contacts;
+    }
 }
+
+
+
+
